991a618a3a629b4d574427a772db4e143712e5a1
junyong300@gmail.com <junyong300@gmail.com>
2022-08-03 18:13:12 +0900

    init

---
+++ utils.py
@@ -0,0 +1,42 @@
+""" Collection of Utilities"""
+import os
+import yaml
+from torch import nn
+
+def read_yaml(str):
+    """read yaml files into object"""
+    f = open(str, 'r')
+    cfg = yaml.safe_load(f)
+    print("====Yaml Configuration====")
+    for c in cfg:
+        print(c,":", cfg[c])
+    print("==========================")
+    return cfg
+
+class ECELoss(nn.Module):
+    def __init__(self, n_bins=15):
+        """
+        n_bins (int): number of confidence interval bins
+        """
+        super(ECELoss, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits, labels):
+        softmaxes = F.softmax(logits, dim=1)
+        confidences, predictions = torch.max(softmaxes, 1)
+        accuracies = predictions.eq(labels)
+
+        ece = torch.zeros(1, device=logits.device)
+        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+            # Calculated |confidence - accuracy| in each bin
+            in_bin = confidences.gt(
+                bin_lower.item()) * confidences.le(bin_upper.item())
+            prop_in_bin = in_bin.float().mean()
+            if prop_in_bin.item() > 0:
+                accuracy_in_bin = accuracies[in_bin].float().mean()
+                avg_confidence_in_bin = confidences[in_bin].mean()
+                ece += torch.abs(
+                    avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+        return ece
